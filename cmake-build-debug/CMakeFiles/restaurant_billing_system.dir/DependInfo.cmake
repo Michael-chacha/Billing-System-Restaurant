25b9fb32831a08720e23cb7b1fc2aad614f9d338
Michael-Chacha <michaelchacha@gmail.com>
2025-01-02 16:58:16 +0300

    Propery set up the project to RUn

---
+++ cmake-build-debug/CMakeFiles/restaurant_billing_system.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/priver/Studies/restaurant_billing_system/admin.cpp" "CMakeFiles/restaurant_billing_system.dir/admin.cpp.o" "gcc" "CMakeFiles/restaurant_billing_system.dir/admin.cpp.o.d"
+  "/home/priver/Studies/restaurant_billing_system/main.cpp" "CMakeFiles/restaurant_billing_system.dir/main.cpp.o" "gcc" "CMakeFiles/restaurant_billing_system.dir/main.cpp.o.d"
+  "/home/priver/Studies/restaurant_billing_system/menu.cpp" "CMakeFiles/restaurant_billing_system.dir/menu.cpp.o" "gcc" "CMakeFiles/restaurant_billing_system.dir/menu.cpp.o.d"
+  "/home/priver/Studies/restaurant_billing_system/user.cpp" "CMakeFiles/restaurant_billing_system.dir/user.cpp.o" "gcc" "CMakeFiles/restaurant_billing_system.dir/user.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/restaurant_billing_system.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/restaurant_billing_system.dir/admin.cpp.o"
+  "CMakeFiles/restaurant_billing_system.dir/admin.cpp.o.d"
+  "CMakeFiles/restaurant_billing_system.dir/main.cpp.o"
+  "CMakeFiles/restaurant_billing_system.dir/main.cpp.o.d"
+  "CMakeFiles/restaurant_billing_system.dir/menu.cpp.o"
+  "CMakeFiles/restaurant_billing_system.dir/menu.cpp.o.d"
+  "CMakeFiles/restaurant_billing_system.dir/user.cpp.o"
+  "CMakeFiles/restaurant_billing_system.dir/user.cpp.o.d"
+  "build/restaurant_billing_system"
+  "build/restaurant_billing_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/restaurant_billing_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
